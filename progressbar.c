e94d130de4a5679d5e457b662f847e8611e59a08
Benedek Szabó <bndkszabo@gmail.com>
2024-02-05 16:08:30 +0100

    Just uploaded to hub

---
+++ progressbar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void progressbar(int progress, int total, int length) {
+    int j;
+    float progressPercent = (float) progress / total * 100;
+    printf("Progress: [");
+    for (j = 0; j < length; j++) { // a középső szám állítja a vonal hosszúgágát
+        if (j < progressPercent * length / 100) {
+            printf("■");
+        } else {
+            printf(" ");
+        }
+    }
+    printf("] %.0f%%\033[A\n", progressPercent);
+    fflush(stdout);
+    // Sleep for 100 milliseconds
+}
+
+int main() {
+    int i;
+    int max_value = 100;
+    int length = 50;
+    for (i = 0; i <= max_value; i++) {
+        progressbar(i, max_value, length);
+    }
+    printf("\n");
+
+    return 0;
+}
+
